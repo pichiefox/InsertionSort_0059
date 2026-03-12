f9d00d704f45f0545d7144d334b6d911a0942b6b
pichiefox <citrazad@gmail.com>
2026-03-12 18:26:05 +0700

    procedure input

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int arr[20];
 int n;
+
+void input (){
+    while (true){
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=========================================";
+    cout << "==========Masukan Element Array==========";
+    cout << "=========================================";
+
+    for (int i = 0; i < n; i++ ){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
